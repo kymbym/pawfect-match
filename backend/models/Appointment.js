40e383f5ae5ac4a08c1769ba372648e994000e4e
uncommonlibrary <carmen916@gmail.com>
2024-08-29 16:59:02 +0800

    feat: added appt and user models

---
+++ backend/models/Appointment.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const appointmentSchema = new mongoose.Schema({
+  adopter: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  pet: { type: mongoose.Schema.Types.ObjectId, ref: "Pet" },
+  provider: { type: mongoose.Schema.Types.ObjectId, ref: "Partner" },
+  contact: { type: String, required: true },
+  appointmentDate: { type: Date, required: true },
+  appointmentTime: { type: Number, required: true },
+  inquiries: String,
+});
+
+const Appointment = mongoose.model("Appointment", appointmentSchema);
+
+module.exports = Appointment;
---
+++ frontend/components/SignupForm/UserSignupForm.jsx
@@ -0,0 +1,88 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function UserSignUpForm() {
+  const navigate = useNavigate();
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPw: "",
+  });
+
+  const handleChange = (event) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      [event.target.name]: event.target.value,
+    }));
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    try {
+      console.log(formData);
+      navigate("/home/userId");
+    } catch (error) {
+      console.error("Error submitting form:", error);
+    }
+  };
+
+  const { name, email, password, confirmPw } = formData;
+
+  const isFormInvalid = () => {
+    return !(name && email && password && password === confirmPw);
+  };
+
+  return (
+    <>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Name:{" "}
+          <input
+            type="text"
+            id="name"
+            value={name}
+            name="name"
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Email:{" "}
+          <input
+            type="email"
+            id="email"
+            value={email}
+            name="email"
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Password:{" "}
+          <input
+            type="password"
+            id="password"
+            value={password}
+            name="password"
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Confirm Password:{" "}
+          <input
+            type="password"
+            id="confirmPw"
+            value={confirmPw}
+            name="confirmPw"
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <br />
+        <button disabled={isFormInvalid()}>Submit</button>
+      </form>
+    </>
+  );
+}
