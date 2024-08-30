9e8f60c930ba8166cf62865b19682c6ab6290b23
uncommonlibrary <carmen916@gmail.com>
2024-08-30 10:22:06 +0800

    chore: post signup user

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -2,5 +2,64 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
+const User = require("../models/User");
+const { verifyToken } = require("../middleware/user-verify-token");
 
-router.post("/signup");
+const SALT_LENGTH = 12;
+
+router.post("/signup", async (req, res) => {
+  const { userName, email, hashedPassword } = req.body;
+  try {
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ error: "This account already exists!" });
+    }
+
+    const hashedPassword = await bcrypt.hash(
+      req.body.hashedPassword,
+      SALT_LENGTH,
+    );
+    const user = await User.create({ userName, email, hashedPassword });
+    const token = jwt.sign({ user }, process.env.JWT_SECRET, {
+      expiresIn: "5m", //after testing, change to "1h"
+    });
+    res.status(201).json({ user, token });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+router.post("/login", async (req, res) => {
+  const { userName, email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (user && bcrypt.compare({ password }, user.hashedPassword)) {
+      const token = jwt.sign({ user }, process.env.JWT_SECRET, {
+        expiresIn: "5m",
+      });
+      res.status(200).json({ token });
+    } else {
+      res.status(401).json({ error: "Invalid username or password" });
+    }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+router.get("/:userId", verifyToken, async (req, res) => {
+  try {
+    if (req.user._id !== req.params.userId) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
+    const user = await User.findById(req.user._id);
+    if (!user) {
+      res.status(404);
+      throw new Error("Please sign in again.");
+    }
+    res.json({ user });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,24 @@
 const dotenv = require("dotenv");
 dotenv.config();
 const express = require("express");
-const app = express();
+const morgan = require("morgan");
 const mongoose = require("mongoose");
-app.use(express.static("../frontend/dist"));
+const usersRouter = require("./controllers/users");
+const User = require("./models/User");
 
 mongoose.connect(process.env.MONGODB_URI);
-
 mongoose.connection.on("connected", () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
+const app = express();
+
+app.use(morgan("dev"));
+app.use(express.static("../frontend/dist"));
 app.use(express.json());
 
 //routes here:
-app.use("/user", usersRouter);
+app.use("/api/user", usersRouter);
 
 app.listen(3000, () => {
   console.log("The express app is ready!");
