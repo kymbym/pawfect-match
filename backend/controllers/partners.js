fe736249f83594a1a1988f012a721bbe5c3ecdeb
kymbym <kymmyfong@gmail.com>
2024-08-30 15:47:20 +0800

    implement delete pet function

--- backend/controllers/partners.js
+++ backend/controllers/partners.js
@@ -96,19 +96,16 @@ router.post("/pets", verifyToken, async (req, res) => {
   } catch (error) {
     res.status(400).json({ error: error.message })
   }
-})
+});
 
 // edit pet 
 router.put("/pets/:petId", verifyToken, async (req, res) => {
   const { petId } = req.params;
   const updates = req.body;
 
-  console.log("Updating pet with ID:", petId);
-  console.log("Updates:", updates);
-
   try {
     if (!mongoose.Types.ObjectId.isValid(petId)) {
-      return res.status(400).json({ msg: "invalid pet id" })
+      return res.status(400).json({ msg: "invalid pet id" });
     }
 
     const pet = await Pet.findById(petId)
@@ -121,9 +118,35 @@ router.put("/pets/:petId", verifyToken, async (req, res) => {
     }
 
     const updatedPet = await Pet.findByIdAndUpdate(petId, updates, { new: true });
-    res.status(200).json({ msg: "pet updated successfully ", updatedPet })
+    res.status(200).json({ msg: "pet updated successfully ", updatedPet });
   } catch (error) {
-    res.status(400).json({ error: error.message })
+    res.status(400).json({ error: error.message });
+  }
+});
+
+// delete pet
+router.delete("/pets/:petId", verifyToken, async (req, res) => {
+  const { petId } = req.params;
+
+  try {
+    if (!mongoose.Types.ObjectId.isValid(petId)) {
+      return res.status(400).json({ msg: "invalid pet id" });
+    }
+
+    const pet = await Pet.findById(petId)
+
+    if (!pet) {
+      return res.status(404).json({ error: "pet not found" });
+    }
+
+    if (!pet.provider.equals(req.partner._id)) {
+      return res.status(403).json({ error: "unauthorized! not allowed to edit" });
+    }
+
+    const deletedPet = await Pet.findByIdAndDelete(petId);
+    res.status(200).json({ msg: "pet deleted successfully", deletedPet });
+    } catch (error) {
+      res.status(400).json({ error: error.message });
   }
 })
 
