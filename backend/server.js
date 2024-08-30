988adfb550e7ff7583c5363725fbe37e6be6d7da
kymbym <kymmyfong@gmail.com>
2024-08-30 10:49:37 +0800

    fix: fix user auth

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,21 @@
 const dotenv = require("dotenv");
 dotenv.config();
 const express = require("express");
-const morgan = require("morgan");
+const app = express();
 const mongoose = require("mongoose");
 app.use(express.static("../frontend/dist"));
 const partnersRouter = require("./controllers/partners");
+const usersRouter = require("./controllers/users");
 
 mongoose.connect(process.env.MONGODB_URI);
 mongoose.connection.on("connected", () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
-const app = express();
-
-app.use(morgan("dev"));
-app.use(express.static("../frontend/dist"));
 app.use(express.json());
 
 app.use("/api/partner", partnersRouter);
+app.use("/api/user", usersRouter);
 
 app.listen(3000, () => {
   console.log("The express app is ready!");
