0739c0482ab6382184a8e1508d2074db54e525f9
uncommonlibrary <carmen916@gmail.com>
2024-08-30 16:53:27 +0800

    chore: get specific appointment by id

--- backend/controllers/appointments.js
+++ backend/controllers/appointments.js
@@ -6,12 +6,29 @@ const router = express.Router();
 // Protected Routes
 router.use(verifyToken);
 
+//include partner and pet later
+//make appointment:
 router.post("/", async (req, res) => {
   const currentUser = getUser(req);
   req.body.adopter = currentUser._id;
   const appointment = await Appointment.create(req.body);
   appointment._doc.adopter = currentUser;
   res.status(201).json(appointment);
+});
+
+//show all appointments made by me (user):
+router.get("/", async (req, res) => {
+  const allAppointments = await Appointment.find({}).populate("adopter").exec();
+  res.status(200).json(allAppointments);
+});
+
+//view specific appointment:
+router.get("/:appointmentId", async (req, res) => {
+  const { appointmentId } = req.params;
+  const appointment = await Appointment.findById(appointmentId)
+    .populate("adopter")
+    .exec();
+  res.status(200).json(appointment);
 });
 
 module.exports = router;
