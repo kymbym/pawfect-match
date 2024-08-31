5df0ed1eb8af31bc30df287136f33380724ec5aa
kymbym <kymmyfong@gmail.com>
2024-08-31 23:16:35 +0800

    feat: merge verifytoken middleware

--- backend/middleware/verify-token.js
+++ backend/middleware/verify-token.js
@@ -1,12 +1,21 @@
+const Partner = require("../models/Partner")
+const User = require("../models/User")
+
 const jwt = require("jsonwebtoken");
 
 const setPartner = (req, partner) => {
   req.partner = partner;
 };
 
+const setUser = (req, user) => {
+  req.user = user;
+}
+
 const getPartner = (req) => req.partner;
 
-function verifyToken(req, res, next) {
+const getUser = (req) => req.user;
+
+const verifyToken = async (req, res, next) => {
   try {
     const token = req.headers.authorization.split(" ")[1];
     if(!token) {
@@ -16,13 +25,23 @@ function verifyToken(req, res, next) {
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     console.log("decoded token", decoded)
+
+    const partner = await Partner.findById(decoded._id)
+    if (partner) {
+      setPartner(req, partner)
+      return next()
+    }
+
+    const user = await User.findById(decoded._id)
+    if (user) {
+      setUser(req, user)
+      return next()
+    }
     
-    setPartner(req, decoded);
-    next();
   } catch (error) {
     console.error("token verification error backend", error)
     res.status(401).json({ error: "invalid authorization token." });
   }
 }
 
-module.exports = { verifyToken, getPartner };
+module.exports = { verifyToken, getPartner, getUser };
