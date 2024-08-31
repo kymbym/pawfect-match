1c36cbdcfe9bffffa08d6798c0e570baf2b19b5c
uncommonlibrary <carmen916@gmail.com>
2024-09-01 03:23:45 +0800

    feat: created user search page

---
+++ frontend/pages/UserPages/UserSearchPage/UserSearchPage.jsx
@@ -0,0 +1,12 @@
+import UserNavBar from "../../../components/NavBar/UserNavBar";
+
+export default function UserSearchPage() {
+  return (
+    <>
+      <UserNavBar />
+      <h1>Find your bestie!</h1>
+      <p>insert search bar</p>
+      <p>other search functions</p>
+    </>
+  );
+}
