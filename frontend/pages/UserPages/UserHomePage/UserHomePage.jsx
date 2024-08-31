39299cc50370fcc96c49aab920eed7ab81fa86a7
uncommonlibrary <carmen916@gmail.com>
2024-09-01 03:20:46 +0800

    chore: added some content to user homepage

--- frontend/pages/UserPages/UserHomePage/UserHomePage.jsx
+++ frontend/pages/UserPages/UserHomePage/UserHomePage.jsx
@@ -1,7 +1,13 @@
+import UserNavBar from "../../../components/NavBar/UserNavBar"
+
 export default function UserHomePage() {
     return (
-        <>
-        <h1>User Home Page</h1>
-        </>
-    )
+      <>
+        <UserNavBar />
+        <h1>What would you like to do today?</h1>
+        <p>simple card: search pets</p>
+        <p>simple card: view favorites</p>
+        <p>simple card: view appointments</p>
+      </>
+    );
 }
