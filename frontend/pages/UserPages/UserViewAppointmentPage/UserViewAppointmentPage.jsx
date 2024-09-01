cde050b6e9bd691222950d5780087f26d13b63f4
uncommonlibrary <carmen916@gmail.com>
2024-09-01 22:46:54 +0800

    refactor: loadUserAppointments()

--- frontend/pages/UserPages/UserViewAppointmentPage/UserViewAppointmentPage.jsx
+++ frontend/pages/UserPages/UserViewAppointmentPage/UserViewAppointmentPage.jsx
@@ -5,15 +5,18 @@ import { getUserAppointments } from "../../../services/userService";
 export default function UserViewAppointmentPage({ token }) {
   const [appointments, setAppointments] = useState([]);
   // console.log("token in userview page:", token)
-  const loadUserAppointments = async () => {
-    const data = await getUserAppointments(token);
-    setAppointments(data);
-    console.log("current appointments:", appointments);
-  };
 
   useEffect(() => {
+    const loadUserAppointments = async () => {
+      const data = await getUserAppointments(token);
+      setAppointments(data);
+      //   console.log("current appointments:", appointments);
+    };
     loadUserAppointments();
   }, [token]);
+
+  console.log("current appointments:", appointments);
+
   return (
     <>
       <UserNavBar />
@@ -27,7 +30,6 @@ export default function UserViewAppointmentPage({ token }) {
               <h2>Pet Name: {appointment.pet.name}</h2>
               <h3>Date: {appointment.appointmentDate}</h3>
               <h3>Time: {appointment.appointmentTime}</h3>
-              <h3>Provider: {appointment.provider}</h3>
             </div>
           ))}
         </div>
