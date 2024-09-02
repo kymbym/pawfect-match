1ba941331bc7142e2c02f8de7020a9158df68aa5
uncommonlibrary <carmen916@gmail.com>
2024-09-03 03:41:59 +0800

    chore: search by name will display on screen

--- frontend/pages/UserPages/UserSearchPage/UserSearchPage.jsx
+++ frontend/pages/UserPages/UserSearchPage/UserSearchPage.jsx
@@ -2,12 +2,13 @@ import UserNavBar from "../../../components/NavBar/UserNavBar";
 import PetCard from "../../../components/PetCard/PetCard";
 import UserSearchBar from "../../../components/UserSearchBar/UserSearchBar";
 import { useState, useEffect } from "react";
+import {useLocation} from "react-router-dom";
 import { getAllPets } from "../../../services/partnerservices";
 
 export default function UserSearchPage({ token }) {
-
-  const [displayQuery, setdisplayQuery] = useState("");
+  // const [displayQuery, setdisplayQuery] = useState("");
   const [pets, setPets] = useState([]);
+  const location = useLocation();
   const view = "user";
 
   useEffect(() => {
@@ -23,13 +24,18 @@ export default function UserSearchPage({ token }) {
         } else {
           console.error("no pets data found!");
         }
+        if (location.state){
+          console.log("location state result:", location.state.results.pets);
+          const findPetByNameResult = location.state.results.pets;
+          setPets(findPetByNameResult);
+        }
       } catch (error) {
         console.error("error fetching pets", error);
       }
     };
-
-    fetchPets();
-  }, [token]);
+ 
+      fetchPets();
+  }, [token, location.state]);
 
   return (
     <>
