a2d9dc75ff07ca495c56a58d5071f43c26eedefa
kymbym <kymmyfong@gmail.com>
2024-09-05 15:04:03 +0800

    style: user signup form

--- frontend/components/SignupForm/UserSignupForm.jsx
+++ frontend/components/SignupForm/UserSignupForm.jsx
@@ -39,125 +39,107 @@ export default function UserSignupForm() {
 
   return (
     <>
-      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <img
-            alt="A poodle"
-            src="../../../images/sign-up-banner.png"
-            className="mx-auto h-10 w-auto"
-          />
-          <h1 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Create an account
+      <div className="columns">
+        <div className="column is-align-content-center">
+          <h1
+            className="titan-one-regular"
+            style={{ fontSize: "4.5em", margin: "0.1em" }}
+          >
+            Pawfect Match
           </h1>
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+        </div>
+      </div>
+ 
+
+      <div className="columns">
+        <div className="column is-align-content-center">
+          <figure className="image is-16 by 9 ">
+            <iframe
+              width="350"
+              height="180"
+              src="../../../images/sign-up-banner.png"
+              alt="A poodle"
+            />
+          </figure>
+          <h2 className="quattrocento-sans-regular">Create an account</h2>
+          <h3 className="quattrocento-sans-regular">
             Enter your details to create an account.
-          </h2>
+          </h3>
         </div>
 
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form onSubmit={handleSubmit} className="space-y-6">
-            <div>
-              <label
-                htmlFor="userName"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Name:
-              </label>
-              <div className="mt-2">
-                <input
-                  type="text"
-                  id="userName"
-                  value={userName}
-                  name="userName"
-                  onChange={handleChange}
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
+        <div className="column is-half">
+          <div className="field">
+            <label className="label quattrocento-sans-bold">Name</label>
+            <div className="control">
+              <input
+                className="input is-small quattrocento-sans-regular"
+                type="text"
+                placeholder="e.g Jane Doe"
+                value={userName}
+                name="userName"
+                onChange={handleChange}
+              />
             </div>
-            <div>
-              <label
-                htmlFor="email"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Email:
-              </label>
-              <div className="mt-2">
-                <input
-                  type="email"
-                  id="email"
-                  value={email}
-                  name="email"
-                  onChange={handleChange}
-                  autoComplete="email"
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-            <div>
-              <div className="flex items-center justify-between">
-                <label
-                  htmlFor="password"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  Password:
-                </label>
-              </div>
-              <div className="mt-2">
-                <input
-                  type="password"
-                  id="password"
-                  value={password}
-                  name="password"
-                  onChange={handleChange}
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-            <div>
-              <div className="flex items-center justify-between">
-                <label
-                  htmlFor="confirmPw"
-                  className="block text-sm font-medium leading-6 text-gray-900"
-                >
-                  Confirm Password:
-                </label>
-              </div>
-              <div className="mt-2">
-                <input
-                  type="password"
-                  id="confirmPw"
-                  value={confirmPw}
-                  name="confirmPw"
-                  onChange={handleChange}
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-            <br />
-            <div>
-              <button
-                disabled={isFormInvalid()}
-                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Submit
-              </button>
-            </div>
-          </form>
+          </div>
 
-          <p className="mt-10 text-center text-sm text-gray-500">
-            Have an existing account?{" "}
-            <a
-              onClick={handleLoginClick}
-              className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-              style={{ cursor: "pointer" }}
+          <div className="field">
+            <label className="label quattrocento-sans-bold">Email</label>
+            <div className="control">
+              <input
+                className="input is-small quattrocento-sans-regular"
+                type="email"
+                placeholder="e.g. janedoe@mail.com"
+                value={email}
+                name="email"
+                onChange={handleChange}
+              />
+            </div>
+          </div>
+
+          <div className="field">
+            <label className="label quattrocento-sans-bold">Password</label>
+            <div className="control">
+              <input
+                className="input is-small"
+                type="password"
+                value={password}
+                name="password"
+                onChange={handleChange}
+              />
+            </div>
+          </div>
+
+          <div className="field">
+            <label className="label quattrocento-sans-bold">Confirm Password</label>
+            <div className="control">
+              <input
+                className="input is-small"
+                type="password"
+                value={confirmPw}
+                name="confirmPw"
+                onChange={handleChange}
+              />
+            </div>
+          </div>
+
+          <div className="control">
+            <button
+              type="submit"
+              className="button is-primary quattrocento-sans-bold"
+              onClick={handleSubmit}
+              disabled={isFormInvalid()}
+              style={{ margin: "0.3em", background: "#fff4f2" }}
             >
+              Submit
+            </button>
+          </div>
+
+          <h4 style={{ paddingTop: "8px" }} className="quattrocento-sans-regular">
+            Have an existing account?{" "}
+            <u onClick={handleLoginClick} style={{ cursor: "pointer" }} className="quattrocento-sans-regular-italic">
               Login here
-            </a>
-          </p>
+            </u>{" "}
+          </h4>
         </div>
       </div>
     </>
